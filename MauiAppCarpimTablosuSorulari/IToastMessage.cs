5f0853760283c09afd51960b21354a4a27af2ba3
guestdeveloper <guestdeveloper@odakgis.com.tr>
2023-01-01 18:27:24 +0300

    files unnecessary removed

---
+++ MauiAppCarpimTablosuSorulari/IToastMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiAppCarpimTablosuSorulari
+{
+    public interface IToastMessage
+    {
+        void ShortToast(string message);
+        void LongToast(string message);
+    }
+}
---
+++ MauiAppCarpimTablosuSorulari/ToastMessage.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace MauiAppCarpimTablosuSorulari
+{
+    public class ToastMessage : IToastMessage
+    {
+        public void LongToast(string message)
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+            ToastDuration duration = ToastDuration.Long;
+            double fontSize = 32;
+            var toast = Toast.Make(message, duration, fontSize);
+            toast.Show(cancellationTokenSource.Token);
+        }
+
+        public void ShortToast(string message)
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 32;
+            var toast = Toast.Make(message, duration, fontSize);
+            toast.Show(cancellationTokenSource.Token);
+        }
+    }
+}
